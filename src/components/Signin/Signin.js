ec42f48ef7ff0a74594bef536630f39db255d80b
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-09-19 15:39:42 -0400

    fix app.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -17,7 +17,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://guarded-wildwood-64753.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
