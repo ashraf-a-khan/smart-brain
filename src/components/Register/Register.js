1b171168146b1384d8ef49a1622b97faa9559814
ashraf-a-khan <khan.ashraf2311@gmail.com>
2020-09-19 21:58:27 -0400

    deploy

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/register', {
+    fetch('https://guarded-wildwood-64753.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
